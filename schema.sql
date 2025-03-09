e1932858312cfcbb7fee6fd3ef8970bc90ccc1c0
josephsasaki <joseph.sasaki.314@gmail.com>
2025-03-09 18:42:49 +0000

    Added composite keys

--- schema.sql
+++ schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE dancer (
     last_name VARCHAR(32) NOT NULL CHECK (LENGTH(first_name) > 0),
     rank_id SMALLINT NOT NULL,
     PRIMARY KEY (dancer_id),
-    CONSTRAINT fk_rank FOREIGN KEY(rank_id) REFERENCES rank(rank_id),
+    CONSTRAINT fk_rank FOREIGN KEY (rank_id) REFERENCES rank(rank_id),
 )
 
 
@@ -67,8 +67,8 @@ CREATE TABLE act (
     ballet_id SMALLINT NOT NULL,
     act_type_id SMALLINT NOT NULL,
     PRIMARY KEY (act_id),
-    CONSTRAINT fk_ballet FOREIGN KEY(ballet_id) REFERENCES ballet(ballet_id),
-    CONSTRAINT fk_act_type FOREIGN KEY(act_type_id) REFERENCES act_type(act_type_id)
+    CONSTRAINT fk_ballet FOREIGN KEY (ballet_id) REFERENCES ballet(ballet_id),
+    CONSTRAINT fk_act_type FOREIGN KEY (act_type_id) REFERENCES act_type(act_type_id)
 )
 
 CREATE TABLE group (
@@ -77,8 +77,8 @@ CREATE TABLE group (
     act_id SMALLINT NOT NULL,
     gender_id SMALLINT NOT NULL,
     PRIMARY KEY (group_id),
-    CONSTRAINT fk_act FOREIGN KEY(act_id) REFERENCES act(act_id),
-    CONSTRAINT fk_gender FOREIGN KEY(gender_id) REFERENCES gender(gender_id)
+    CONSTRAINT fk_act FOREIGN KEY (act_id) REFERENCES act(act_id),
+    CONSTRAINT fk_gender FOREIGN KEY (gender_id) REFERENCES gender(gender_id)
 )
 
 CREATE TABLE role (
@@ -87,43 +87,61 @@ CREATE TABLE role (
     group_id SMALLINT NOT NULL,
     position_id SMALLINT, -- we would want to check roles and full mirrors within a ballet have unique positions
     PRIMARY KEY (role_id),
-    CONSTRAINT fk_group FOREIGN KEY(group_id) REFERENCES group(group_id),
-    CONSTRAINT fk_position FOREIGN KEY(position_id) REFERENCES position(position_id),
+    CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES group(group_id),
+    CONSTRAINT fk_position FOREIGN KEY (position_id) REFERENCES position(position_id),
 )
 
 --- BALLET OTHER ---
 
 
 CREATE TABLE cast_option (
-    cast_option_id SMALLINT GENERATED ALWAYS AS IDENTITY,
     role_id SMALLINT NOT NULL,
     dancer_id SMALLINT NOT NULL,
-    cast_option_costume VARCHAR(32) DEFAULT 'Default Costume',
-    PRIMARY KEY (cast_option_id),
-    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES role(role_id),
-    CONSTRAINT fk_dancer FOREIGN KEY(dancer_id) REFERENCES dancer(dancer_id)
+    PRIMARY KEY (role_id, dancer_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id),
+    CONSTRAINT fk_dancer FOREIGN KEY (dancer_id) REFERENCES dancer(dancer_id)
 )
 
+-- At any given point, there must be every combination of groups. 
 CREATE TABLE cast_rule (
-    cast_rule_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    ballet_id SMALLINT NOT NULL,
-    rule_type_id SMALLINT NOT NULL,
     group_one_id SMALLINT NOT NULL,
     group_two_id SMALLINT NOT NULL,
-    PRIMARY KEY (cast_rule_id),
-    CHECK (group_one_id <> group_two_id),
-    CONSTRAINT fk_ballet FOREIGN KEY(ballet_id) REFERENCES ballet(ballet_id),
-    CONSTRAINT fk_rule_type FOREIGN KEY(rule_type_id) REFERENCES rule_type(rule_type_id),
-    CONSTRAINT fk_group_one FOREIGN KEY(group_one_id) REFERENCES group(group_id),
-    CONSTRAINT fk_group_two FOREIGN KEY(group_two_id) REFERENCES group(group_id)
+    rule_type_id SMALLINT NOT NULL,
+    PRIMARY KEY (group_one_id, group_two_id),
+    UNIQUE (group_one_id, group_two_id),
+    CONSTRAINT fk_group_one FOREIGN KEY (group_one_id) REFERENCES group(group_id),
+    CONSTRAINT fk_group_two FOREIGN KEY (group_two_id) REFERENCES group(group_id),
+    CONSTRAINT fk_rule_type FOREIGN KEY (rule_type_id) REFERENCES rule_type(rule_type_id)
 )
 
 CREATE TABLE full_mirror (
     full_mirror_id SMALLINT GENERATED ALWAYS AS IDENTITY,
     role_id SMALLINT NOT NULL,
     position_id SMALLINT NOT NULL, -- we would want to check roles and full mirrors within a ballet have unique positions
-    CONSTRAINT fk_position FOREIGN KEY(position_id) REFERENCES position(position_id)
+    PRIMARY KEY (full_mirror_id),
+    CONSTRAINT fk_position FOREIGN KEY (position_id) REFERENCES position(position_id)
 )
 
+--- SHOWS ---
 
---
+
+CREATE TABLE show (
+    show_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    show_name VARCHAR(32) NOT NULL UNIQUE CHECK (LENGTH(show_name) > 0),
+    ballet_id SMALLINT NOT NULL,
+    show_date DATE,
+    PRIMARY KEY (show_id),
+    CONSTRAINT fk_ballet FOREIGN KEY (ballet_id) REFERENCES ballet(ballet_id)
+)
+
+-- At any given point, a show will have all the roles
+CREATE TABLE casting (
+    show_id SMALLINT NOT NULL,
+    role_id SMALLINT NOT NULL,
+    dancer_id SMALLINT,
+    PRIMARY KEY (show_id, role_id),
+    CONSTRAINT fk_show FOREIGN KEY (show_id) REFERENCES show(show_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(role_id),
+    CONSTRAINT fk_dancer FOREIGN KEY (dancer_id) REFERENCES dancer(dancer_id),
+)
+
